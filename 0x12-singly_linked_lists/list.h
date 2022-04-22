2d7d5c8c0325b93cee498538626b67735416e05b
Terrence Peters <terrencepeters54@gmail.com>
2022-04-22 13:16:39 +0100

    new

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,9 +1,11 @@
-#ifndef _LISTS_
-#define _LISTS_
+#ifndef _SINGLY_LINKED_LISTS
+#define _SINGLY_LINKED_LISTS
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <malloc.h>
 
 /**
  * struct list_s - singly linked list
@@ -21,10 +23,15 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
+/*Helper functions*/
+char *strdup(const char *s);
+int _strlen(const char *s);
 
 #endif
